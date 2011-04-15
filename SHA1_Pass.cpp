f10ad367829cd1191a2107581f29856eb2de9794
16s <github@16s.us>
2011-04-15 17:41:39 -0400

    1.5-BETA2

--- SHA1_Pass.cpp
+++ SHA1_Pass.cpp
@@ -17,6 +17,7 @@
 	// Crypto++
 	#include <cryptopp/sha.h>
 	#include <cryptopp/hex.h>
+	#include <cryptopp/hmac.h>
 	#include <cryptopp/base64.h>
 #endif
 
@@ -37,6 +38,7 @@
 
 	#include <sha.h>
 	#include <hex.h>
+	#include <hmac.h>
 	#include <base64.h>
 #endif
 
@@ -51,6 +53,7 @@
 	// Crypto++
 	#include <cryptopp/sha.h>
 	#include <cryptopp/hex.h>
+	#include <cryptopp/hmac.h>
 	#include <cryptopp/base64.h>
 #endif
 
@@ -69,8 +72,14 @@
 //#include "simplified_chinese.hpp"
 
 
-// Set to true for debugging/test build
+// Set to true for debugging/test builds
 const static bool SPDEBUG = false;
+const static bool SPTEST = false;
+
+
+// HMAC_SHA1
+bool HMAC_SHA1 = false;
+const static std::string HMAC_MSG = "SHA1_Pass";
 
 
 // std::string to wxString
@@ -96,7 +105,7 @@ const std::string hex_passwd( const std::string& sentence, const std::string& ty
 	hash.Update((const byte*)sentence.c_str(), sentence.size());
 	hash.Final( (byte*)digest );
 	
-	// Encoding
+	// SHA1 Encoding
 	std::string hex;
 	CryptoPP::HexEncoder hexencoder;
 	hexencoder.Attach( new CryptoPP::StringSink( hex ) );
@@ -105,11 +114,46 @@ const std::string hex_passwd( const std::string& sentence, const std::string& ty
 	
 	// Lowercase
 	transform( hex.begin(), hex.end(), hex.begin(), (int(*)(int)) tolower );
+
+	// HMAC (Sentence is the secret key, string constant "SHA1_Pass" is the message)
+	CryptoPP::HMAC<CryptoPP::SHA1> mac;
+	char hmac[ hash.DigestSize() ];
+	mac.SetKey( (const byte*)sentence.c_str(), sentence.size() );
+	mac.Update( (const byte*)HMAC_MSG.c_str(), HMAC_MSG.size() );
+	mac.Final( (byte*)hmac );
+
+	// HMAC Encoding
+	std::string hmac_hex;
+	CryptoPP::HexEncoder hmac_hexencoder;
+	hmac_hexencoder.Attach( new CryptoPP::StringSink( hmac_hex ) );
+	hmac_hexencoder.Put( (byte*)hmac, sizeof(hmac) );
+	hmac_hexencoder.MessageEnd();
 	
-	if ( type == full )
-		return hex;
+	// Lowercase
+	transform( hmac_hex.begin(), hmac_hex.end(), hmac_hex.begin(), (int(*)(int)) tolower );
+	
+	if ( SPDEBUG == true )
+	{
+		//Show the SHA1 hash and the HMAC_SHA1 hash
+		wxMessageBox( std2wx(hex), sha1_label, wxOK | wxICON_INFORMATION );
+		wxMessageBox( std2wx(hmac_hex), hmac_label, wxOK | wxICON_INFORMATION );
+	}
+	
+	if ( HMAC_SHA1 == true )	
+	{
+		if ( type == full )
+			return hmac_hex;
+		else
+			return hmac_hex.substr( 0, 20 );
+	}
+
 	else
-		return hex.substr( 0, 20 );
+	{
+		if ( type == full )
+			return hex;
+		else
+			return hex.substr( 0, 20 );
+	}
 }
 
 
@@ -122,17 +166,49 @@ const std::string b64_passwd( const std::string& sentence, const std::string& ty
 	hash.Update((const byte*)sentence.c_str(), sentence.size());
 	hash.Final( (byte*)digest );
 	
-	// Encoding
+	// SHA1 Encoding
 	std::string b64;
 	CryptoPP::Base64Encoder b64encoder;
 	b64encoder.Attach( new CryptoPP::StringSink( b64 ) );
 	b64encoder.Put( (byte*)digest, sizeof(digest) );
 	b64encoder.MessageEnd();
 	
-	if ( type == full )
-		return b64;
+	// HMAC (Sentence is the secret key, string constant "SHA1_Pass" is the message)
+	CryptoPP::HMAC<CryptoPP::SHA1> mac;
+	char hmac[ hash.DigestSize() ];	
+	mac.SetKey( (const byte*)sentence.c_str(), sentence.size() );
+	mac.Update( (const byte*)HMAC_MSG.c_str(), HMAC_MSG.size() );
+	mac.Final( (byte*)hmac );
+	
+	// HMAC Encoding
+	std::string hmac_b64;
+	CryptoPP::Base64Encoder hmac_b64encoder;
+	hmac_b64encoder.Attach( new CryptoPP::StringSink( hmac_b64 ) );
+	hmac_b64encoder.Put( (byte*)hmac, sizeof(hmac) );
+	hmac_b64encoder.MessageEnd();
+	
+	if ( SPDEBUG == true )
+	{
+		//Show the SHA1 hash and the HMAC_SHA1 hash
+		wxMessageBox( std2wx(b64), sha1_label, wxOK | wxICON_INFORMATION );
+		wxMessageBox( std2wx(hmac_b64), hmac_label, wxOK | wxICON_INFORMATION );
+	}
+	
+	if ( HMAC_SHA1 == true )
+	{
+		if ( type == full )
+			return hmac_b64;
+		else
+			return hmac_b64.substr( 0, 14 );
+	}
+
 	else
-		return b64.substr( 0, 14 );
+	{
+		if ( type == full )
+			return b64;
+		else
+			return b64.substr( 0, 14 );
+	}
 }
 
 
@@ -202,6 +278,7 @@ class MyFrame: public wxFrame
 		void OnHexHalf( wxCommandEvent& event );
 		void OnViewSentence( wxCommandEvent& event );
 		void OnSecureMode( wxCommandEvent& event );
+		void OnHMAC( wxCommandEvent& event );
 		void OnHelp( wxCommandEvent& event );
 		void rbt_clear_cb();
 		void rbt_clear_s();
@@ -219,6 +296,7 @@ class MyFrame: public wxFrame
 		// Check boxes
 		wxCheckBox view_sentence;
 		wxCheckBox secure_mode;
+		wxCheckBox hmac;
 	
 		// Buttons
 		wxButton help;
@@ -244,6 +322,7 @@ enum
 	ID_HexHalf,
 	ID_ViewSentence,
 	ID_SecureMode,
+	ID_HMAC,
 	ID_Help
 	
 };
@@ -257,6 +336,7 @@ EVT_MENU( ID_Hex, MyFrame::OnHex )
 EVT_MENU( ID_HexHalf, MyFrame::OnHexHalf )
 EVT_MENU( ID_ViewSentence, MyFrame::OnViewSentence )
 EVT_MENU( ID_SecureMode, MyFrame::OnSecureMode )
+EVT_MENU( ID_SecureMode, MyFrame::OnHMAC )
 EVT_MENU( ID_Help, MyFrame::OnHelp )
 END_EVENT_TABLE()
 
@@ -277,7 +357,7 @@ bool MyApp::OnInit()
 	frame->SetSizeHints( 436,150, 436,150, 0 );
 	
 	// Run tests
-	if ( SPDEBUG == true )
+	if ( SPTEST == true )
 	{
 		tests();
 	}
@@ -301,23 +381,26 @@ MyFrame::MyFrame( const wxString& title, const wxPoint& pos, const wxSize& size,
 	
 	secure_mode.Create( panel, 4, sm, wxPoint( 194, 35 ), wxDefaultSize, 0, wxDefaultValidator );	
 	Connect( 4, wxEVT_COMMAND_CHECKBOX_CLICKED, wxCommandEventHandler( MyFrame::OnSecureMode ) );
-	
-	partial_pass.Create( panel, 5, wxEmptyString, wxPoint( 309, 33 ), wxSize( 90, 25 ), wxTE_READONLY, wxDefaultValidator );
 
-	HEX.Create( panel, 6, hex_label, wxPoint( 69, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
-	Connect( 6, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnHex ) );
+	hmac.Create( panel, 5, hm, wxPoint( 270, 35 ), wxDefaultSize, 0, wxDefaultValidator );	
+	Connect( 5, wxEVT_COMMAND_CHECKBOX_CLICKED, wxCommandEventHandler( MyFrame::OnHMAC ) );
 	
-	HEXH.Create( panel, 7, hex_half_label, wxPoint( 153, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
-	Connect( 7, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnHexHalf ) );
+	partial_pass.Create( panel, 6, wxEmptyString, wxPoint( 349, 33 ), wxSize( 50, 25 ), wxTE_READONLY, wxDefaultValidator );
+
+	HEX.Create( panel, 7, hex_label, wxPoint( 69, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
+	Connect( 7, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnHex ) );
 	
-	B64.Create( panel, 8, b64_label, wxPoint( 235, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
-	Connect( 8, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnBase64 ) );
+	HEXH.Create( panel, 8, hex_half_label, wxPoint( 153, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
+	Connect( 8, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnHexHalf ) );
 	
-	B64H.Create( panel, 9, b64_half_label, wxPoint( 317, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
-	Connect( 9, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnBase64Half ) );
+	B64.Create( panel, 9, b64_label, wxPoint( 235, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
+	Connect( 9, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnBase64 ) );
 	
-	help.Create( panel, 10, help_label, wxPoint( 403, 5 ), wxSize( 25, 25), wxBU_EXACTFIT, wxDefaultValidator );	
-	Connect( 10, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnHelp ) );
+	B64H.Create( panel, 10, b64_half_label, wxPoint( 317, 62 ),  wxSize( 80, 25 ), wxBU_EXACTFIT, wxDefaultValidator );	
+	Connect( 10, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnBase64Half ) );
+	
+	help.Create( panel, 11, help_label, wxPoint( 403, 5 ), wxSize( 25, 25), wxBU_EXACTFIT, wxDefaultValidator );	
+	Connect( 11, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame::OnHelp ) );
 	
 	// On start-up, set partial_pass value
 	partial_pass.SetValue( pass_peek );
@@ -327,6 +410,9 @@ MyFrame::MyFrame( const wxString& title, const wxPoint& pos, const wxSize& size,
 	
 	// On start-up, do not use Secure Mode
 	secure_mode.SetValue( false );
+
+	// On start-up, do not use HMAC
+	hmac.SetValue( false );
 	
 	// The 1 means only create one status field within the statusbar
 	CreateStatusBar( 1, wxFULL_REPAINT_ON_RESIZE, wxID_ANY );
@@ -342,6 +428,7 @@ MyFrame::MyFrame( const wxString& title, const wxPoint& pos, const wxSize& size,
 	B64H.SetToolTip( a_14_char_tip );
 	view_sentence.SetToolTip( vs_tip );
 	secure_mode.SetToolTip( sm_tip );
+	hmac.SetToolTip( hmac_tip );
 	help.SetToolTip( help_tip );
 	
 	//Populate About Information
@@ -406,8 +493,8 @@ void MyFrame::OnBase64( wxCommandEvent& WXUNUSED( event ) )
 	// Convert clean_password to wxString
 	wxString the_password = std2wx( clean_password );
 	
-	// Display partial pass (first 6 chars of password)
-	partial_pass.SetValue( the_password.SubString( 0, 5 ) );
+	// Display partial pass (first 4 chars of password)
+	partial_pass.SetValue( the_password.SubString( 0, 3 ) );
 	
 	// Copy the password to the clipboard.
 	if ( wxTheClipboard->Open() )
@@ -422,7 +509,7 @@ void MyFrame::OnBase64( wxCommandEvent& WXUNUSED( event ) )
 	if ( SPDEBUG == true )
 	{
 		wxMessageBox( the_sentence, u_sentence, wxOK | wxICON_INFORMATION, this);
-		wxMessageBox( the_password, b64_label, wxOK | wxICON_INFORMATION, this);
+		wxMessageBox( the_password, password, wxOK | wxICON_INFORMATION, this);
 		
 		// Log password		
 		wxTextFile file( debug_b64 );		
@@ -454,8 +541,8 @@ void MyFrame::OnBase64Half( wxCommandEvent& WXUNUSED( event ) )
 	// Convert clean_password to wxString
 	wxString the_password = std2wx( clean_password );
 	
-	// Display partial pass (first 6 chars of password)
-	partial_pass.SetValue( the_password.SubString( 0, 5 ) );
+	// Display partial pass (first 4 chars of password)
+	partial_pass.SetValue( the_password.SubString( 0, 3 ) );
 	
 	// Copy the password to the clipboard.
 	if ( wxTheClipboard->Open() )
@@ -470,7 +557,7 @@ void MyFrame::OnBase64Half( wxCommandEvent& WXUNUSED( event ) )
 	if ( SPDEBUG == true )
 	{
 		wxMessageBox( the_sentence, u_sentence, wxOK | wxICON_INFORMATION, this);
-		wxMessageBox( the_password, b64_half_label, wxOK | wxICON_INFORMATION, this);
+		wxMessageBox( the_password, password, wxOK | wxICON_INFORMATION, this);
 		
 		// Log password		
 		wxTextFile file( debug_b64_h );		
@@ -502,8 +589,8 @@ void MyFrame::OnHex( wxCommandEvent& WXUNUSED( event ) )
 	// Convert clean_password to wxString
 	wxString the_password = std2wx( clean_password );
 	
-	// Display partial pass (first 6 chars of password)
-	partial_pass.SetValue( the_password.SubString( 0, 5 ) );
+	// Display partial pass (first 4 chars of password)
+	partial_pass.SetValue( the_password.SubString( 0, 3 ) );
 	
 	// Copy the password to the clipboard.
 	if ( wxTheClipboard->Open() )
@@ -518,7 +605,7 @@ void MyFrame::OnHex( wxCommandEvent& WXUNUSED( event ) )
 	if ( SPDEBUG == true )
 	{
 		wxMessageBox( the_sentence, u_sentence, wxOK | wxICON_INFORMATION, this);
-		wxMessageBox( the_password, hex_label, wxOK | wxICON_INFORMATION, this);
+		wxMessageBox( the_password, password, wxOK | wxICON_INFORMATION, this);
 		
 		// Log password		
 		wxTextFile file( debug_hex );		
@@ -550,8 +637,8 @@ void MyFrame::OnHexHalf( wxCommandEvent& WXUNUSED( event ) )
 	// Convert clean_password to wxString
 	wxString the_password = std2wx( clean_password );
 
-	// Display partial pass (first 6 chars of password)
-	partial_pass.SetValue( the_password.SubString( 0, 5 ) );
+	// Display partial pass (first 4 chars of password)
+	partial_pass.SetValue( the_password.SubString( 0, 3 ) );
 	
 	// Copy the password to the clipboard.
 	if ( wxTheClipboard->Open() )
@@ -566,7 +653,7 @@ void MyFrame::OnHexHalf( wxCommandEvent& WXUNUSED( event ) )
 	if ( SPDEBUG == true )
 	{
 		wxMessageBox( the_sentence, u_sentence, wxOK | wxICON_INFORMATION, this);
-		wxMessageBox( the_password, hex_half_label, wxOK | wxICON_INFORMATION, this);
+		wxMessageBox( the_password, password, wxOK | wxICON_INFORMATION, this);
 		
 		// Log password
 		wxTextFile file( debug_hex_h );
@@ -659,3 +746,22 @@ void MyFrame::OnSecureMode( wxCommandEvent& WXUNUSED( event ) )
 		partial_pass.SetValue( pass_peek );
 	}
 }
+
+
+void MyFrame::OnHMAC( wxCommandEvent& WXUNUSED( event ) )
+{
+	// HMAC has been checked.
+	if ( hmac.IsChecked() )
+	{
+		SetStatusText( hmac_on );
+		HMAC_SHA1 = true;
+	}
+
+	// HMAC has been unchecked.
+	else
+	{
+		SetStatusText( hmac_off );
+		HMAC_SHA1 = false;
+	}
+}
+
